5a190c3c0bd6fbf29f0337b6e45748365d87d5e6
fr1g <cheunghast@outlook.com>
2026-03-08 22:45:29 +0300

    修复文本换行问题

--- src/comps/Bubble.tsx
+++ src/comps/Bubble.tsx
@@ -2,9 +2,9 @@
 
 
 export default function Bubble({ message, opposite = false, extras }: { message: string, opposite?: boolean, extras?: string }) {
-    return <div className={`grid p-0.5 ${opposite ? 'justify-items-start' : 'justify-items-end'}`}>
-        <div className={`${opposite ? 'rounded-bl-xs bg-blue-50 dark:bg-slate-800' : 'rounded-tr-xs bg-blue-300 dark:bg-slate-500'} shadow p-2 rounded-xl `}>
-            <div className={`${opposite ? '' : ''}`}>{message}</div>
+    return <div className={`grid? w-full p-0.5 ${opposite ? 'justify-items-start' : 'justify-items-end'}`}>
+        <div className={`${opposite ? 'rounded-bl-xs bg-blue-50 dark:bg-slate-800' : 'rounded-tr-xs bg-blue-300 dark:bg-blue-400/60'} max-w-full w-fit shadow p-2 rounded-xl `}>
+            <pre className={`${opposite ? '' : ''} whitespace-pre-wrap font-sans! w-full`}>{message}</pre>
             {
                 extras && <div className={`${opposite ? '' : 'text-right'} text-xs/4`}>
                     {extras}
--- src/pages/conversation.tsx
+++ src/pages/conversation.tsx
@@ -7,28 +7,44 @@ import ClickableAvatar from "../comps/ClickableAvatar";
 
 
 export function Conversation() {
-    const [chatStack, setChatStack] = useState<Array<number>>([]);
+    const [chatStack, setChatStack] = useState<Array<string>>([]);
     const { chatId } = useParams();
     const reuses = useContext(ReusableFuncs);
 
-    const chatArea = useRef(null)
+    const chatArea = useRef(null);
+    const input = useRef(null);
+    const send = useRef(null);
 
     useEffect(() => {
         setTimeout(() => {
-            if (chatStack.length < 18) setChatStack(_ => [..._, _.length]);
+            if (chatStack.length < 18) setChatStack(_ => [..._, `${_.length}`]);
         }, 1500);
     });
+
+    useEffect(() => {
+        if (input) {
+
+        }
+    }, [input])
 
     useEffect(() => {
         if (chatArea.current) {
             let n = (chatArea.current as HTMLDivElement);
             n.scrollTo({ top: n.scrollHeight, behavior: 'smooth' });
         }
+        console.log(chatStack)
     }, [chatStack, chatArea]);
+
+    const [text, setText] = useState<null | string>(null);
+    const updateInput = (val: string) => {
+        const trimmed = val.trim();
+        if (trimmed.length <= 0) setText(null);
+        setText(trimmed);
+    }
 
     if (chatId) return <>
         <div className=" size-full flex flex-col relative">
-            <div className="-translate-y-0.5 flex rounded-lg bg-slate-100 dark:bg-slate-600 items-center gap-1.5 p-2 shadow-md">
+            <div className=" flex rounded-lg bg-slate-100 dark:bg-slate-600 items-center gap-1.5 p-2 shadow-md">
                 <div onClick={() => reuses?.goHome()} className="border-button grid items-center justify-items-center sm:hidden aspect-square">
                     <ChevronLeftIcon className="block" fillColor='transparent' strokeColor='currentColor' strokeWidth={2} />
                 </div>
@@ -38,14 +54,40 @@ export function Conversation() {
                 </div>
                 <ClickableAvatar className="" />
             </div>
-            <div ref={chatArea} className="flex-col-reverse flex grow h-full overflow-x-hidden overflow-y-scroll py-3">
+            <div ref={chatArea} className="flex-col-reverse flex rounded-lg grow h-full overflow-x-hidden overflow-y-scroll py-3 relative">
                 {[...chatStack].reverse().map((msg) => {
-                    return <Bubble message={`${msg}`} opposite={msg % 2 == 0} key={msg} extras="2026/mar/7-10:24" />
+                    let tryparse = parseFloat(msg); // 临时的预览逻辑
+                    return <Bubble message={`${msg}`} opposite={!isNaN(tryparse) && (tryparse % 2 == 0)} key={msg} extras="2026/mar/7-10:24" />
                 })}
             </div>
-            <div className="w-full min-h-12 flex gap-1.5 sm:gap-3 mt-1  bottom-0 left-0 right-0">
-                <textarea style={{ resize: 'none' }} name="textingbox" id="txt" className=" rounded-lg size-full grow focus:outline-2 focus:outline-slate-400 shadow-md bg-slate-100/80 dark:bg-slate-800/50 p-1.5"></textarea>
-                <div className="bg-blue-400/50 hover:bg-blue-400/75 active:bg-blue-300/45 grid shadow-md items-center justify-items-center rounded-lg  aspect-square">
+            <div className="w-full min-h-14 text-sm sm:min-h-20 sm:text-base flex gap-1.5 sm:gap-3 mt-1  bottom-0 left-0 right-0">
+                <textarea
+                    onKeyUp={(e) => {
+                        if (e.key.toLowerCase() != 'enter') return;
+                        else if (e.key.toLowerCase() == 'enter' && e.shiftKey) return;
+                        else if (send.current && text) (send.current as HTMLDivElement).click();
+                    }}
+                    onChange={(e) => updateInput(e.target.value)}
+                    ref={input}
+                    style={{ resize: 'none' }}
+                    name="textingbox"
+                    id="txt"
+                    className=" rounded-lg size-full grow focus:outline-2 
+                    focus:outline-slate-400 shadow-md bg-slate-100/80 
+                    dark:bg-slate-800/50 p-1.5"
+                    enterKeyHint="send"
+                ></textarea>
+                <div ref={send}
+                    className={`
+                        ${text == null ? 'bg-gray-300/80 cursor-not-allowed! pointer-events-none' : 'bg-blue-400/50 hover:bg-blue-400/75 active:bg-blue-300/45'}
+                     grid shadow-md items-center justify-items-center rounded-lg  aspect-square
+                    `} onClick={() => {
+                        if (text) {
+                            setChatStack(_ => [..._, text]);
+                            setText(null);
+                            if (input.current) (input.current as HTMLTextAreaElement).value = "";
+                        }
+                    }}>
                     <SendIcon className="block" fillColor='transparent' strokeColor='currentColor' strokeWidth={2} />
                 </div>
             </div>
